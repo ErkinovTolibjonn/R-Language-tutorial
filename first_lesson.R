7eef7eb6fcd88e91df5d6f3afb6f45a1c71308ae
ErkinovTolibjonn <aimlprogrammer015@gmail.com>
2023-02-02 09:54:23 +0500

    First lesson

---
+++ first_lesson.R
@@ -0,0 +1,13 @@
+#R comment
+
+# "Hello World"  # This is comment
+
+# R variable
+
+name <- "Tolibjon"
+
+age <- 21
+
+print(name) # print the value of the name variable
+
+print(age) # print the value of the age variable
