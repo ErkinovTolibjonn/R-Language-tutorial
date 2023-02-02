a6eab2dc9df317de0013eee29798584a09a5a9b2
ErkinovTolibjonn <aimlprogrammer015@gmail.com>
2023-02-02 21:07:29 +0500

    Second lesson

---
+++ second_lesson.R
@@ -0,0 +1,39 @@
+# Concatenate elements
+
+x <- "R is programming language"
+
+y <- " and it is awesome"
+
+print(paste(x,y))
+
+# Assign the same value to multiple variables in one line
+
+var <- var1 <- var2 <- "Orange"
+
+print(var)
+
+print(var1)
+
+print(var2)
+
+# Legal variable  name
+
+var_x <- 10
+
+Var_x <- 5
+
+varX <- 7
+
+var_x2 <- (-6)
+
+.var1 <- 9
+
+print(var_x)
+
+print(Var_x)
+
+print(varX)
+
+print(var_x2)
+
+print(.var1)
