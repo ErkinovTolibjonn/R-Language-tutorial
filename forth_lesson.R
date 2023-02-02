af663bd1baa0d034424fec8aa73f81df4d3d4fab
ErkinovTolibjonn <aimlprogrammer015@gmail.com>
2023-02-02 21:44:45 +0500

    Forth lesson

---
+++ forth_lesson.R
@@ -0,0 +1,27 @@
+#R simple math
+
+x <- (-10)
+
+y <- 5
+
+z <- 16
+
+print(x+y)  # plus operation
+
+print(x-y)  # minus operation
+
+print(abs(x))  # absolute value
+
+print(sqrt(z)) # square root of number
+
+print(min(x,y,z)) # min value
+
+print(max(x,y,z)) # max value
+
+# ceiling() and floor() function
+
+a <- 1.4
+
+print(ceiling(a)) #function rounds a number upwards to its nearest integer
+
+print(floor(b)) #function rounds a number downwards to its nearest integer
