28fa6b8a2f88bf6dc9f64e04df488e248df887ec
ErkinovTolibjonn <aimlprogrammer015@gmail.com>
2023-02-03 20:11:07 +0500

    Fifth lesson

---
+++ fifth_lesson.R
@@ -0,0 +1,39 @@
+'''If you want the line breaks 
+to be inserted at the same position 
+as in the code, use the cat() function:'''
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+print(cat(str))
+
+# string length
+
+print(nchar(str))
+
+'''Use the grepl() function 
+to check if a character or a sequence
+ of characters are present in a string:'''
+
+str <- "Hello World!"
+
+print(grepl("H", str))
+print(grepl("Hello", str))
+print(grepl("X", str))
+
+'''Use the paste() function to merge/concatenate two strings:'''
+x <- "Hello"
+
+y <- "world"
+
+print(paste(x,y))
+
+# Escape charakters in R
+
+#  \\   backslash
+#  \n   new line
+#  \r   carriage return
+#  \t   tab
+#  \b   backspace
